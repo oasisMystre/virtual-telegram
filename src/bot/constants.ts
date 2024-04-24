1b268aa694c18afa0fb3123113a9b370c7a79bda
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-04-24 10:44:10 +0100

    Update constants.ts

--- src/bot/constants.ts
+++ src/bot/constants.ts
@@ -3,7 +3,7 @@ export const CREATE_NEW_NUMBER_WIZARD = "CREATE_NEW_NUMBER_WIZARD";
 
 export const OTP_ACTION = "otp";
 export const REJECT_ACTION = "reject";
-export const CREATE_VIRTUAL_NUMBER_ACTION = "create";
+export const CREATE_VIRTUAL_NUMBER_ACTION = "phone";
 
 
 export const WALLET_COMMAND = "wallet";
